d628d31a5404313d51cc8284b35695f6439a2cea
pedro henrique <dnlsniperonly2@gmail.com>
2025-06-09 20:14:28 +0000

    resolve sem quebrar o código por favor To resolve the issue, you need to

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>✅ Site funcionando com Pages Router</h1>
-    </main>
-  );
-}
